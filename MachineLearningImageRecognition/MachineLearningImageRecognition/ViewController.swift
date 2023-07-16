679f9033f9c3b2d49103cf827fc31d4309a8c9c7
ierolsen <ierolsen@outlook.com>
2023-07-16 11:41:58 +0300

    initial commit

---
+++ MachineLearningImageRecognition/MachineLearningImageRecognition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MachineLearningImageRecognition
+//
+//  Created by Erol Sen on 16.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
